f1323ad02eb4ae95d43db0f9c1f64adcd0067c1d
hq20051252 <hq20051252@163.com>
2015-03-11 11:59:00 +0800

    构建词库，小脚本

--- words/tr.py
+++ words/tr.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8-*-
+
+import sys
+
+
 def strQ2B(ustring):
     """全角转半角"""
     rstring = ""
     for uchar in ustring:
         inside_code=ord(uchar)
-        if inside_code == 12288:                              #全角空格直接转换            
-            inside_code = 32 
-        elif (inside_code >= 65281 and inside_code <= 65374): #全角字符（除空格）根据关系转化
+        # 全角空格直接转换
+        if inside_code == 12288:
+            inside_code = 32
+        # 全角字符（除空格）根据关系转化
+        elif (inside_code >= 65281) and (inside_code <= 65374):
             inside_code -= 65248
 
         rstring += unichr(inside_code)
     return rstring
-    
+
+
 def strB2Q(ustring):
     """半角转全角"""
     rstring = ""
     for uchar in ustring:
         inside_code=ord(uchar)
-        if inside_code == 32:                                 #半角空格直接转化                  
+        # 半角空格直接转化
+        if inside_code == 32:
             inside_code = 12288
-        elif inside_code >= 32 and inside_code <= 126:        #半角字符（除空格）根据关系转化
+        # 半角字符（除空格）根据关系转化
+        elif (inside_code >= 32) and (inside_code <= 126):
             inside_code += 65248
 
         rstring += unichr(inside_code)
     return rstring
+
 
 def test():
 
@@ -35,9 +45,16 @@ def test():
     print c.encode("utf-8")
 
 
+def usage():
+    print "python tr.py <filename>"
+    sys.exit(1)
+
 
 def main():
-    f = "./classes.txt"
+    if len(sys.argv) == 2:
+        f = sys.argv[1]
+    else:
+        usage()
 
     with open(f, "rb") as fd:
         for line in fd.xreadlines():
